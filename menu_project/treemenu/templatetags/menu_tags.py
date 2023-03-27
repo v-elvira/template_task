30a7a612cd47d9fdece87a5cbb4671c7454ce43f
elya <vavina.elvira@gmail.com>
2023-03-27 07:28:35 +0300

    Format

--- menu_project/treemenu/templatetags/menu_tags.py
+++ menu_project/treemenu/templatetags/menu_tags.py
@@ -7,30 +7,6 @@ from django.utils.html import format_html
 
 register = template.Library()
 
-# Recursively generate html for item and its children
-def draw_item(item, menu_dict, top_parent, active_parents):
-    is_active = item == top_parent
-
-    menu_html = ['<li{}>'.format(' class="has_children"' if menu_dict[item] else ''),]
-    if is_active:
-        menu_html.append('<span class="active">')
-    if item.url:
-        menu_html.append('<a href="{}">{}</a>'.format(item.url, item.label))
-    elif item.named_url:
-        menu_html.append('<a href="{}">{}</a>'.format(reverse(item.named_url), item.label))
-    else:
-        menu_html.append('{}'.format(item.label))
-    if is_active:
-        menu_html.append('</span>')
-
-    if is_active and menu_dict[item]:
-        menu_html.append('<ul>')
-        current_parent =  None if not len(active_parents) else active_parents.pop()
-        for child_item in menu_dict[item]:
-            menu_html.extend(draw_item(child_item, menu_dict, current_parent, active_parents))
-        menu_html.append('</ul>')
-    menu_html.append('</li>')
-    return menu_html
 
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
@@ -70,3 +46,30 @@ def draw_menu(context, menu_name):
         menu_html.extend(draw_item(item, menu_dict, top_parent, active_parents))
     menu_html.append('</ul>')
     return format_html(''.join(menu_html))
+
+
+# Recursively generate html for item and its children
+def draw_item(item, menu_dict, top_parent, active_parents):
+    is_active = item == top_parent
+
+    menu_html = ['<li{}>'.format(' class="has_children"' if menu_dict[item] else ''),]
+    if is_active:
+        menu_html.append('<span class="active">')
+    if item.url:
+        menu_html.append('<a href="{}">{}</a>'.format(item.url, item.label))
+    elif item.named_url:
+        menu_html.append('<a href="{}">{}</a>'.format(reverse(item.named_url), item.label))
+    else:
+        menu_html.append('{}'.format(item.label))
+    if is_active:
+        menu_html.append('</span>')
+
+    if is_active and menu_dict[item]:
+        menu_html.append('<ul>')
+        current_parent =  None if not len(active_parents) else active_parents.pop()
+        for child_item in menu_dict[item]:
+            menu_html.extend(draw_item(child_item, menu_dict, current_parent, active_parents))
+        menu_html.append('</ul>')
+    menu_html.append('</li>')
+    return menu_html
+    
